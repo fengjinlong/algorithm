d2bd3def43d87d6999973b711bde0ee96eca4b8f
fjl <18618166564@163.com>
2021-08-23 09:44:43 +0800

    1

---
+++ src/排序/快排.js
@@ -0,0 +1,33 @@
+const arr = [1,7,2,6,4,9,5];
+function quick(left, right) {
+  let i = left
+  let j = right
+  if (i > j) {
+    return
+  }
+  let tem = arr[i];
+  while (i < j) {
+    while(tem <= arr[j] && i < j) {
+      j--
+    }
+    while(tem >= arr[i] && i < j) {
+      i++;
+    }
+    if (i < j) {
+      let t = arr[i];
+      arr[i] = arr[j];
+      arr[j] = t;
+    }
+  }
+  arr[left] = arr[i];
+  arr[i] = tem;
+
+  quick(left, i-1);
+  quick(i+1, right);
+  console.log(arr);
+  
+  // quick(_left, _right)
+}
+
+quick(0, 6)
+
