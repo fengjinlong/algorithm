df405b89c5e9f2856eb0c584bdff932db7432d85
fjl <18618166564@163.com>
2021-09-10 14:01:07 +0800

    1

--- src/test/index.spec.js
+++ src/test/index.spec.js
@@ -1,12 +1,23 @@
-
-import {Queue} from '../队列/queue'
+import {
+  Queue
+} from '../队列/queue'
 describe('init', () => {
   it('ii', () => {
     const queue = new Queue()
+    const eliminateList = []
     queue.enqueue(1)
-    queue.enqueue(1)
-    expect(queue.size()).toBe(2);
-    queue.dequeue()
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.enqueue(4)
+    while (queue.size() > 1) {
+      for (let i = 0; i < 5; i++) {
+        queue.enqueue(queue.dequeue())
+      }
+      eliminateList.push(queue.dequeue())
+    }
+
     expect(queue.size()).toBe(1);
+    queue.dequeue()
+    expect(queue.size()).toBe(0);
   })
 })
