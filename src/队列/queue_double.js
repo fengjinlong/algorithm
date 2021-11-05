be7c99ef2e1ffb62cbe9ff8a305e51f8bb1c0eee
fjl <18618166564@163.com>
2021-11-05 10:02:00 +0800

    1

--- src/队列/queue_double.js
+++ src/队列/queue_double.js
@@ -36,54 +36,83 @@ class Queue_double {
     this.heightIndex--;
     this.count++;
   }
+  // toString
   eachAll() {
     let len = this.count
     let arr = []
+    let j = this.heightIndex + 1
     for (var i = 0; i < len; i++) {
-      let j = this.heightIndex + 1
       arr.push(this.items[j])
-      this.heightIndex++
+      j++
     }
     return arr.join(' ')
   }
-  // del
-  // dequeue() {
-  //   if (this.isEmpty()) {
-  //     return undefined;
-  //   }
-  //   const result = this.items[this.lowestCount]
-  //   delete this.items[this.lowestCount]
-  //   this.lowestCount++;
-  //   return result;
-  // }
-  // peek() {
-  //   if (this.isEmpty) {
-  //     return undefined;
-  //   }
-  //   return this.items[this.lowestCount]
-  // }
+  // del height
+  del_queue_height() {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    this.heightIndex++
+    const result = this.items[this.heightIndex]
+    delete this.items[this.heightIndex]
+    this.count--
+    console.log(result)
+    return result
+
+  }
+  // del low
+  del_queue_low() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    this.lowestIndex--
+    const result = this.items[this.lowestIndex]
+    delete this.items[this.lowestIndex]
+    this.count--
+    console.log(result)
+    return result;
+  }
+  peek_height() {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    
+
+  }
+  peek_low() {
+    if (this.isEmpty) {
+      return undefined;
+    }
+    return this.items[this.lowestCount]
+  }
   isEmpty() {
-    return this
+    return this.count === 0
   }
   size() {
     return this.count
   }
 
-  // clear() {
-  //   this.items = {}
-  //   this.count = 0
-  //   this.lowestCount = 0
-  // }
+  clear() {
+    this.items = {}
+    this.count = 0
+    this.lowestIndex = 0
+    this.heightIndex = 0
+  }
 }
 
 let q = new Queue_double()
+// console.log(q.isEmpty())
 q.enqueue_low(1)
 q.enqueue_low(2)
 q.enqueue_heihgt(3)
 q.enqueue_low(4)
 q.enqueue_heihgt(5)
 q.enqueue_low(6)
-
+// console.log(q.isEmpty())
 // 531246
-
+console.log(q.eachAll())
+q.del_queue_low()
+// console.log(1)
+console.log(q.eachAll())
+q.del_queue_height()
 console.log(q.eachAll())
