87e156e5cdd9bc6ca70c7c20fa2b6b9549ddfb30
fjl <18618166564@163.com>
2021-11-09 14:20:56 +0800

    希尔排序

---
+++ src/排序/希尔排序/shell.js
@@ -0,0 +1,26 @@
+const shellSort = (arr) => {
+  let len = arr.length;
+  let temp;
+  let group = Math.floor(len / 2);
+
+  for (group; group > 0; group = Math.floor(group / 2)) {
+
+    for (let i = group; i < len; i++) {
+
+      for (let j = i - group; j >= 0; j = j - group) {
+        let a1 = arr[j];
+        let a2 = arr[j + group];
+        if (arr[j] > arr[j + group]) {
+          temp = arr[j]
+          arr[j] = arr[j + group]
+          arr[j + group] = temp
+        }
+      }
+    }
+  }
+  console.log(arr)
+
+}
+
+const aa = [1, 8, 2, 7, 3, 6, 4, 9,5]
+shellSort(aa)
