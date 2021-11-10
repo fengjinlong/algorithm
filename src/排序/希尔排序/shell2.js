2d92412390c659f830e7349a26b12c30a4a90b6e
fjl <18618166564@163.com>
2021-11-10 10:42:45 +0800

    shell

--- src/排序/希尔排序/shell2.js
+++ src/排序/希尔排序/shell2.js
@@ -1,28 +1,30 @@
 const shellSort = (arr) => {
-  let len = arr.length;
-  let tem;
-  let group = len / 2
-  // 一次分组 最小为1组
+  // 数组长度
+  let length = arr.length;
+  // 变换位置的临时变量
+  let temp;
+  // 把数组分成几组的标记,也就是增量
+
+  let group = Math.floor(length / 2);
+  // 分组必须大于 1 且为整数
   for (group; group > 0; group = Math.floor(group / 2)) {
-    // 以 group 为 2 举例
-    // 分成2组 要比较五次
-
-    // i++ 是为了
-    for (let i = group; i< len; i++) {
-      // 为了分组2组，每组内元素依次比较
-      // i          2   3   4     5    6 
-      //待比较 j     0   1   2 0   3 1  4 2 0
-      //待比较 j+2   2   3   4 2   5 3  6 4 2
-
-      for (let j = i-group; j>=0; j=j-group) {
+    // i-g
+    for (let i = group; i < length; i++) {
+      // j 是一个元素的下标，所有可以为0
+      // j-group 为了  但确定 在 2,3完成比较后，是否影响之前的比较结果。
+      // 比如 1，2 位置已经完成比较。2，3完成比较后这要检查3位置是否影响之前1，2位置的结果
+      for (let j = i - group; j >= 0; j = j - group) {
         if (arr[j] > arr[j+group]) {
-          tem = arr[j]
+          // 换位置
+          temp = arr[j]
           arr[j] = arr[j+group]
-          arr[j+group] = tem
+          arr[j+group] = temp
         }
       }
     }
   }
+
+
   console.log(arr)
 }
 const aa = [10, 8, 2, 7, 3, 6, 4, 9, 5, 1]
