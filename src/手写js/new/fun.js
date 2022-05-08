451e6febbca201d1da481d8e1cd7f28f1bb832e4
fjl <18618166564@163.com>
2022-05-08 21:47:44 +0800

    n

--- src/手写js/new/fun.js
+++ src/手写js/new/fun.js
@@ -1,23 +1,43 @@
-function Fun (name) {
+function Fun(name) {
   this.name = name;
   // return 1
   // return '1'
-  return true
+  return true;
   // return null
 
   // err
   // return {}
   // return function () {}
   // return []
-  return /w/
+  // return /w/
 }
-Fun.prototype.age = 'prototype age'
-Fun.prototype.say = function() {
-  console.log('prototype function say')
-}
+Fun.prototype.age = "prototype age";
+Fun.prototype.say = function () {
+  console.log("prototype function say");
+};
 
-let f = new Fun('name')
-console.log(f)
-console.log(f.name)
-console.log(f.age)
-console.log(f.say())
+let f = new Fun("name");
+console.log(f);
+console.log(f.name);
+console.log(f.age);
+console.log(f.say());
+
+function _new(ctor, ...args) {
+  if (typeof ctor !== "function") {
+    throw new TypeError("ctor must be a function");
+  }
+  let obj = {};
+  obj.__proto__ = ctor.prototype;
+  let result = ctor.apply(obj, [...args]);
+  if (typeof result === "function" || typeof result === "object") {
+    return result;
+  } else {
+    return obj;
+  }
+}
+console.log('_f------------')
+let _f = _new(Fun, 'name');
+console.log(_f);
+console.log(_f.name);
+console.log(_f.age);
+console.log(_f.say());
