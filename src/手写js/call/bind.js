940b636d1dd077a41702326ba684d16c7e949114
fjl <18618166564@163.com>
2022-06-02 07:02:20 +0800

    bind instance

--- src/手写js/call/bind.js
+++ src/手写js/call/bind.js
@@ -65,3 +65,18 @@ Function.prototype.bind4 = function (ctx, ...args) {
   }
   return bound;
 };
+Function.prototype.bind5 = function (ctx, ...args) {
+  if (typeof ctx !== "function") {
+    return new Error("this must be a function");
+  }
+  let fn = this;
+  const bound = function () {
+    let context = this instanceof bound ? this : ctx;
+    let args2 = args.concat(Array.slice.call(arguments));
+    fn.apply(context, args);
+  };
+  if (this.prototype) {
+    Object.setPrototypeOf(bound, this.prototype);
+  }
+  return bound;
+};
---
+++ src/手写js/call/instance.js
@@ -0,0 +1,13 @@
+function myInstance(left, right) {
+  let prototype = right.prototype;
+  left = left.__proto__;
+  while (1) {
+    if (left === null || left === undefined) {
+      return false;
+    }
+    if (prototype === left) {
+      return true;
+    }
+    left = left.__proto__;
+  }
+}
